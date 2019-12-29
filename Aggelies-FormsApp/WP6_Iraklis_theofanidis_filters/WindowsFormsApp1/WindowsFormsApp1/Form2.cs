238bb0162923dbb1ae05d417db7b0a89d7a92c7d
Hercules <56442488+it164664@users.noreply.github.com>
2019-12-29 22:25:05 +0200

    Enimerwsi

--- Aggelies-FormsApp/WP6_Iraklis_theofanidis_filters/WindowsFormsApp1/WindowsFormsApp1/Form2.cs
+++ Aggelies-FormsApp/WP6_Iraklis_theofanidis_filters/WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -19,9 +19,32 @@ namespace WindowsFormsApp1
             InitializeComponent();
            
             this.label1.Text = Form1.dokimi.ToString();
+            if(Form1.dokimi==0) 
+            {
+                dataGridView1.Hide();
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dataSet2.product' table. You can move, or remove it, as needed.
+            this.productTableAdapter1.Fill(this.dataSet2.product);
+            // TODO: This line of code loads data into the 'dataSet11.product' table. You can move, or remove it, as needed.
+            this.productTableAdapter.Fill(this.dataSet11.product);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void productBindingSource1_CurrentChanged(object sender, EventArgs e)
         {
 
         }
