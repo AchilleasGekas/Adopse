ad39288f652ebae03be2ddb3d782f52c4bf9b2f6
Hercules <56442488+it164664@users.noreply.github.com>
2019-12-29 17:16:16 +0200

    Ebala ena arxiko solution gia ta filtra mexri na einai etoimi i vasi

---
+++ Aggelies-FormsApp/WP6_Iraklis_theofanidis_filters/WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public string UserName { get; set; }
+        
+        public Form2()
+        {
+            InitializeComponent();
+           
+            this.label1.Text = Form1.dokimi.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
