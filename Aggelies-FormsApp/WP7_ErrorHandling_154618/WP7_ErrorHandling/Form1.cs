a36d646241d9328f432f523aeae3fedfc0290dc0
BabisTsaga <bill1.dim2@gmail.com>
2020-01-12 12:27:00 +0200

    Log file Error Handling

---
+++ Aggelies-FormsApp/WP7_ErrorHandling_154618/WP7_ErrorHandling/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WP7_ErrorHandling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener("AdholicsErrors.log"));
+            Trace.AutoFlush = true;
+            Trace.Indent();
+            Trace.TraceInformation("Information");
+            Trace.TraceError("Error");
+            Trace.TraceWarning("Warning");
+            Trace.Unindent();
+        }
+    }
+}
---
+++ Aggelies-FormsApp/WP7_ErrorHandling_154618/WP7_ErrorHandling/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WP7_ErrorHandling
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            Trace.Listeners.Add(new TextWriterTraceListener("AdholicsErrors.log"));
+            Trace.AutoFlush = true;
+            Trace.Unindent();
+            Trace.WriteLine("Adholics Closed!!");
+            Trace.Unindent();
+           
+           
+        }
+    }
+}
